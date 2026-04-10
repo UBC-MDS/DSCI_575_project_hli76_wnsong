aa636a026af85d8bec6267a41adafd0aa49e86a2
Harrison Li <hli76@student.ubc.ca>
2026-04-10 10:14:43 -0700

    test streamlit dataframe display

--- app/app.py
+++ app/app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas as pd
 
 st.title("Simple Reactive App")
 
@@ -10,3 +11,8 @@ if user_input:
     st.write(f"You typed: {user_input}")
 else:
     st.write("Start typing above 👆")
+
+st.write(pd.DataFrame({
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 30, 40]
+}))
