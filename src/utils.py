d34784a95592f857cd367c44493506867da3c190
Harrison Li <hli76@student.ubc.ca>
2026-04-12 04:49:46 -0700

    remove stop words while tokenizing

--- src/utils.py
+++ src/utils.py
@@ -1,8 +1,17 @@
 import re
+from nltk.corpus import stopwords
+
+
+STOP_WORDS = set(stopwords.words("english"))
 
 
 def simple_tokenize(text):
     """tokenize document"""
     text = text.lower()
     text = re.sub(r"[^a-z0-9\s-]", "", text)
-    return text.split()
+    tokens = text.split()
+    tokens = [
+        t for t in tokens
+        if t and t not in STOP_WORDS # remove stop words and empty token
+    ]
+    return tokens
