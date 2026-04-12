952b3b4eba004e4ac7ea98c9d7573b80f8a5beef
Harrison Li <hli76@student.ubc.ca>
2026-04-12 02:08:29 -0700

    reorganize script

--- src/download_data.py
+++ src/download_data.py
@@ -21,79 +21,6 @@ os.makedirs(RAW_DATA_DIR, exist_ok=True)
 os.makedirs(PROCESSED_DATA_DIR, exist_ok=True)
 
 
-c2 = duckdb.connect()
-
-review_data_file = f"{CATEGORY}_reviews_raw.parquet"
-meta_data_file = f"{CATEGORY}_meta_raw.parquet"
-
-# save reviews data
-if review_data_file not in os.listdir(RAW_DATA_DIR):
-    print("Downloading Review Data\nCategory: {}".format(CATEGORY))
-    c2.execute(
-        f"""
-          COPY (SELECT rating, title, text, parent_asin, helpful_vote FROM read_json_auto('{REVIEWS_URL}'))
-          TO '{RAW_DATA_DIR}/{review_data_file}'
-          (FORMAT PARQUET, COMPRESSION ZSTD)
-      """
-    )
-    print("Done")
-else:
-    print("Review data for {} already downloaded".format(CATEGORY))
-
-# save meta data
-if meta_data_file not in os.listdir(RAW_DATA_DIR):
-    print("Downloading Meta Data\nCategory: {}".format(CATEGORY))
-    c2.execute(
-        f"""
-    COPY (SELECT * EXCLUDE (images, videos) FROM read_json_auto('{META_URL}', union_by_name=true))
-    TO '{RAW_DATA_DIR}/{meta_data_file}'
-    (FORMAT PARQUET, COMPRESSION ZSTD)
-    """
-    )
-    print("Done")
-else:
-    print("Meta data for {} already downloaded".format(CATEGORY))
-
-
-# merge meta data and reviews and save
-merged_data_file = f"{CATEGORY}_merged.parquet"
-
-if merged_data_file not in os.listdir(PROCESSED_DATA_DIR):
-    print("Merging Review/Meta Data\nCategory: {}".format(CATEGORY))
-    c2.execute(
-        f"""
-        COPY (
-            SELECT r.*, m.title AS product_title, m.price,
-                        m.average_rating, m.main_category, m.store
-            FROM read_parquet('{RAW_DATA_DIR}/{review_data_file}') r
-            LEFT JOIN read_parquet('{RAW_DATA_DIR}/{meta_data_file}') m USING (parent_asin)
-        )
-        TO '{PROCESSED_DATA_DIR}/{merged_data_file}' (FORMAT PARQUET, COMPRESSION ZSTD)
-    """
-    )
-    print("Done")
-else:
-    print("Merged data for {} is ready".format(CATEGORY))
-
-
-products = c2.execute(
-    f"""
-    SELECT 
-        parent_asin,
-        ANY_VALUE(product_title) AS product_title,
-        ANY_VALUE(main_category) AS main_category,
-        ANY_VALUE(store) AS store,
-        ANY_VALUE(price) AS price,
-        ANY_VALUE(average_rating) AS avg_rating,
-        LIST(text) AS reviews,
-        LIST(title) AS review_titles,
-        LIST(helpful_vote) AS helpful_votes
-    FROM read_parquet('{PROCESSED_DATA_DIR}/{merged_data_file}')
-    GROUP BY parent_asin;
-"""
-).df()
-
-
 # build document for a single oroduct
 def build_document(product, max_reviews=20, min_len=30, max_chars=None):
     reviews = product["reviews"]
@@ -140,30 +67,108 @@ Reviews:
     return doc
 
 
-# Download product documents as pickle file
-document_id_file = f"{CATEGORY}_doc_ids.pkl"
-documents_file = f"{CATEGORY}_product_documents.pkl"
+def download_data():
+    c2 = duckdb.connect()
 
-if document_id_file not in os.listdir(PROCESSED_DATA_DIR):
-    print("Downloading document ids\nCategory: {}".format(CATEGORY))
+    review_data_file = f"{CATEGORY}_reviews_raw.parquet"
+    meta_data_file = f"{CATEGORY}_meta_raw.parquet"
 
-    doc_ids = products["parent_asin"].tolist()
+    # save reviews data
+    if review_data_file not in os.listdir(RAW_DATA_DIR):
+        print("Downloading Review Data\nCategory: {}".format(CATEGORY))
+        c2.execute(
+            f"""
+            COPY (SELECT rating, title, text, parent_asin, helpful_vote FROM read_json_auto('{REVIEWS_URL}'))
+            TO '{RAW_DATA_DIR}/{review_data_file}'
+            (FORMAT PARQUET, COMPRESSION ZSTD)
+        """
+        )
+        print("Done")
+    else:
+        print("Review data for {} already downloaded".format(CATEGORY))
 
-    with open(f"{PROCESSED_DATA_DIR}/{document_id_file}", "wb") as f:
-        pickle.dump(doc_ids, f)
+    # save meta data
+    if meta_data_file not in os.listdir(RAW_DATA_DIR):
+        print("Downloading Meta Data\nCategory: {}".format(CATEGORY))
+        c2.execute(
+            f"""
+        COPY (SELECT * EXCLUDE (images, videos) FROM read_json_auto('{META_URL}', union_by_name=true))
+        TO '{RAW_DATA_DIR}/{meta_data_file}'
+        (FORMAT PARQUET, COMPRESSION ZSTD)
+        """
+        )
+        print("Done")
+    else:
+        print("Meta data for {} already downloaded".format(CATEGORY))
 
-    print("Done")
-else:
-    print("Document id for {} is ready".format(CATEGORY))
+    # merge meta data and reviews and save
+    merged_data_file = f"{CATEGORY}_merged.parquet"
 
-if documents_file not in os.listdir(PROCESSED_DATA_DIR):
-    print("Downloading documents\nCategory: {}".format(CATEGORY))
+    if merged_data_file not in os.listdir(PROCESSED_DATA_DIR):
+        print("Merging Review/Meta Data\nCategory: {}".format(CATEGORY))
+        c2.execute(
+            f"""
+            COPY (
+                SELECT r.*, m.title AS product_title, m.price,
+                            m.average_rating, m.main_category, m.store
+                FROM read_parquet('{RAW_DATA_DIR}/{review_data_file}') r
+                LEFT JOIN read_parquet('{RAW_DATA_DIR}/{meta_data_file}') m USING (parent_asin)
+            )
+            TO '{PROCESSED_DATA_DIR}/{merged_data_file}' (FORMAT PARQUET, COMPRESSION ZSTD)
+        """
+        )
+        print("Done")
+    else:
+        print("Merged data for {} is ready".format(CATEGORY))
 
-    documents = list(map(build_document, products.to_dict("records")))
+    # Download product documents as pickle file
+    document_id_file = f"{CATEGORY}_doc_ids.pkl"
+    documents_file = f"{CATEGORY}_product_documents.pkl"
 
-    with open(f"{PROCESSED_DATA_DIR}/{documents_file}", "wb") as f:
-        pickle.dump(documents, f)
+    if document_id_file not in os.listdir(
+        PROCESSED_DATA_DIR
+    ) or documents_file not in os.listdir(PROCESSED_DATA_DIR):
+        products = c2.execute(
+            f"""
+            SELECT 
+                parent_asin,
+                ANY_VALUE(product_title) AS product_title,
+                ANY_VALUE(main_category) AS main_category,
+                ANY_VALUE(store) AS store,
+                ANY_VALUE(price) AS price,
+                ANY_VALUE(average_rating) AS avg_rating,
+                LIST(text) AS reviews,
+                LIST(title) AS review_titles,
+                LIST(helpful_vote) AS helpful_votes
+            FROM read_parquet('{PROCESSED_DATA_DIR}/{merged_data_file}')
+            GROUP BY parent_asin;
+        """
+        ).df()
 
-    print("Done")
-else:
-    print("Document id for {} is ready".format(CATEGORY))
+    if document_id_file not in os.listdir(PROCESSED_DATA_DIR):
+        print("Downloading document ids\nCategory: {}".format(CATEGORY))
+
+        doc_ids = products["parent_asin"].tolist()
+
+        with open(f"{PROCESSED_DATA_DIR}/{document_id_file}", "wb") as f:
+            pickle.dump(doc_ids, f)
+
+        print("Done")
+    else:
+        print("Document id for {} is ready".format(CATEGORY))
+
+    if documents_file not in os.listdir(PROCESSED_DATA_DIR):
+        print("Downloading documents\nCategory: {}".format(CATEGORY))
+
+        documents = list(map(build_document, products.to_dict("records")))
+
+        with open(f"{PROCESSED_DATA_DIR}/{documents_file}", "wb") as f:
+            pickle.dump(documents, f)
+
+        print("Done")
+    else:
+        print("Document id for {} is ready".format(CATEGORY))
+
+
+if __name__ == "__main__":
+    download_data()
